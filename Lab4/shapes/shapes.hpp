23c62f06990728217a14041a1c3f0796dc3dde3e
Gaba5454 <125345499+Gaba5454@users.noreply.github.com>
2023-12-15 08:38:19 +0700

    Create shapes.hpp

---
+++ Lab4/shapes/shapes.hpp
@@ -0,0 +1,22 @@
+#ifndef SHAPES_HPP
+#define SHAPES_HPP
+
+/* ТОЧКА
+ * ОКРУЖНОСТЬ
+ * ЭЛЛИПС
+ * ПРЯМОУГОЛЬНИК
+ * РОМБ
+ * ТРЕУГОЛЬНИК
+ * ОТРЕЗОК */
+
+#include "shape.hpp"    // General
+
+#include "point.hpp"
+#include "circle.hpp"
+#include "ellipse.hpp"
+#include "rectangle.hpp"
+#include "line.hpp"
+#include "triangle.hpp"
+#include "rhombus.hpp"
+
+#endif // SHAPES_HPP
